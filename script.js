8708302ab72e7f7f4de9cd05a17b31b1f601f597
Kenneth Chen <74205780+Kenneth-W-Chen@users.noreply.github.com>
2023-10-31 10:35:55 -0500

    Images now display full size as intended

--- script.js
+++ script.js
@@ -1,54 +1,20 @@
 // ==UserScript==
 // @name        Discord Full Size Image
-// @namespace   Discord_Scripts_K
+// @namespace   Violentmonkey Scripts
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @match       https://discord.com/channels/*
 // @inject      into content
 // @grant       none
-// @version     1.0
+// @version     0.1.0
 // @author      -
 // @description Force full image size load in preview pane on Discord
 // ==/UserScript==
-console.log("loaded")
-const config = { attributes: false, childList: true, subtree: false };
-const configAttr = {attributes: true, childList: false, subtree: false};
-//this regex should remove all GET params
-const imgRegex = /\?[a-z0-9=&]*$/gi;
-// this regex should remove only the image dimension GET params
-const imgRegexDim = /\&width=[0-9]+&height=[0-9]+/gi;
 
-//callback that's run when the page loads the main content; adds mutationobserver to observe the chat container
-const foo = ()=>
+//function to remove the styles and get params that force a small dimension in discord's image preview
+function removeDim(imgWrapperNode)
 {
-  // Check to see if the chat container was updated
-  const searchNode = document.querySelector(".notAppAsidePanel__9d124 > .layerContainer_d5a653:nth-child(4)");
-  mutationObserver.observe(searchNode, config);
-}
-
-const changeImageDimensions = (mutationsList, observer)=>
-{
-  console.log("Mutation")
-  for(mutations of mutationsList)
-  {
-    console.log(mutations)
-    if(mutations.type!=="childList")
-      continue;
-    for(addedNode of mutations.addedNodes)
-    {
-      console.log("Added nodes:")
-      console.log(addedNode)
-      if(addedNode.className !== "layer_ad604d")
-        continue;
-      console.log("Removing info")//.notAppAsidePanel__9d124 > div:nth-child(4)
-      // in theory, this should give the parent of the img
-      // html structure below:
-      //div.layer_ad604d > div.focusLock__10aa5 > div.modal_d2e7a7.root_a28985.fullscreenOnMobile__96797.rootWithShadow__073a7
-        // > div.wrapper__8e1d7 > div.imageWrapper_fd6587.image__79a29
-
-      let imgWrapperNode = addedNode.childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0];
-
-      //remove width and height properties
+   //remove width and height properties
       imgWrapperNode.style.removeProperty("width");
       imgWrapperNode.style.removeProperty("height");
       let imgNode = imgWrapperNode.childNodes[0];
@@ -64,21 +30,66 @@ const changeImageDimensions = (mutationsList, observer)=>
       console.log(imgNode.src)
       imgNode.src = imgNode.src.replace(imgRegex,'');
       console.log(imgNode.src)
-      mutationObserverTwo.observe(imgNode, configAttr);
+}
+
+const config = { attributes: false, childList: true, subtree: false };
+const configChild = {attributes:true,childList:true,subtree:false};
+//this regex should remove all GET params
+const imgRegex = /\?[a-z0-9=&]*$/gi;
+// this regex should remove only the image dimension GET params
+const imgRegexDim = /\&width=[0-9]+&height=[0-9]+/gi;
+
+//callback that's run when the page loads the main content; adds mutationobserver to observe the chat container
+const foo = ()=>
+{
+  // Check to see if the chat container was updated
+  const searchNode = document.querySelector(".notAppAsidePanel__9d124 > .layerContainer_d5a653:nth-child(4)");
+  mutationObserver.observe(searchNode, config);
+}
+
+const changeImageDimensions = (mutationsList, observer)=>
+{
+  for(mutations of mutationsList)
+  {
+    if(mutations.type!=="childList")
+      continue;
+    for(addedNode of mutations.addedNodes)
+    {
+      if(addedNode.className !== "layer_ad604d")
+        continue;
+      //.notAppAsidePanel__9d124 > div:nth-child(4)
+      // in theory, this should give the parent of the img
+      // html structure below:
+      //div.layer_ad604d > div.focusLock__10aa5 > div.modal_d2e7a7.root_a28985.fullscreenOnMobile__96797.rootWithShadow__073a7
+        // > div.wrapper__8e1d7 > div.imageWrapper_fd6587.image__79a29
+      let wrapper = addedNode.childNodes[0].childNodes[0].childNodes[0].childNodes[0];
+      mutationObserverThree.observe(wrapper,configChild);
+      let imgWrapperNode = wrapper.childNodes[0];
+      removeDim(imgWrapperNode);
       break;
     }
     for(removedNode of mutations.removedNodes)
       if(removedNode.className === 'layer_ad604d')
-        mutationObserverTwo.disconnect();
+        {
+          mutationObserverTwo.disconnect();
+          mutationObserverThree.disconnect();
+        }
   }
 }
 
-const attrChange = (mutationsList, observer)=>
+const nodeRemoved = (mutationsList, observer)=>
 {
-  console.log("Attr change")
-  console.log(mutationsList)
+  for(mutations of mutationsList)
+    {
+      console.log(mutations);
+      if(mutations.target.className === 'imageWrapper_fd6587 image__79a29' && !(mutations.addedNodes === undefined || mutations.addedNodes.length==0))
+      {
+        observer.disconnect();
+        removeDim(mutations.target);
+      }
+    }
 }
 
 let mutationObserver = new MutationObserver(changeImageDimensions)
-let mutationObserverTwo = new MutationObserver(attrChange)
+let mutationObserverThree = new MutationObserver(nodeRemoved)
 waitForKeyElements("div.layerContainer_d5a653", foo);
