d5a6f5726e7ce98208891e4e09688dd7a826ad46
Kenneth Chen <74205780+Kenneth-W-Chen@users.noreply.github.com>
2026-04-26 16:15:11 -0500

    Update to work with carousels

--- imgutil.user.js
+++ imgutil.user.js
@@ -8,7 +8,7 @@
 // @downloadurl   https://github.com/Kenneth-W-Chen/discord-full-size-image/raw/main/imgutil.user.js
 // @inject      into content
 // @grant       none
-// @version     0.1.21
+// @version     0.1.22
 // @author      Kenneth-W-Chen
 // @description Force full image size load in preview pane on Discord
 // ==/UserScript==
@@ -16,7 +16,7 @@ const debug = false;
 const appContainerSelector = '.notAppAsidePanel_a3002d > .layerContainer__59d0d:nth-child(n+5)'
 const imageWrapperClass = 'imageWrapper'
 const videoWrapperClass = 'videoWrapper_aa8ea9' //only needed for carousels with videos
-const carouselSelector = '.carouselModal_d3a6f0'
+const carouselSelector = '[class^=mediaArea]' // for carousel
 const imagePopUpLayerParentClass = 'layer_bc663c' // removed when closing image/carousel
 const userPanelClass = 'section[class^="panels"]' // the part of the UI with username, status, pfp, mute, deafen, and settings
 
